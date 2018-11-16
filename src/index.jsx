78300e04dcbcb98554ffc94132bd34f9d3bfa321
David Mortkowitz <dmortkowitz@gmail.com>
2018-11-16 11:34:15 -0800

    Updated content on index jsx file.

--- src/index.jsx
+++ src/index.jsx
@@ -4,3 +4,25 @@ import App from './components/App';
 import { AppContainer } from 'react-hot-loader';
 import { HashRouter } from 'react-router-dom';
 import bootstrap from '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <HashRouter>
+        <Component/>
+      </HashRouter>
+    </AppContainer>,
+    document.getElementById('react-app-root')
+  );
+};
+
+render(App);
+
+/*eslint-disable */
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    render(App);
+  });
+}
+/*eslint-enable */
