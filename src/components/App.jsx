10ca89c040f9390cc8c5b3df5665c7b3e29fe7cf
David Mortkowitz <dmortkowitz@gmail.com>
2018-11-16 10:11:00 -0800

    Add App parent component, and ready components for mainpage, the carousel component inside, and the  marketplace.

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import Carousel from './Carousel';
+
+
+function App(){
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import { AppContainer } from 'react-hot-loader';
+import { HashRouter } from 'react-router-dom';
+import bootstrap from '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
