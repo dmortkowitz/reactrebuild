a144e1afc967259c5762184bb95a6cf7f91c557e
David Mortkowitz <dmortkowitz@gmail.com>
2018-11-16 10:47:33 -0800

    Add MarketplaceList content.

---
+++ src/components/MarketplaceList.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Marketplace from './Marketplace';
+
+
+var MarketplaceList = [
+  {
+    author: Haruki Murakami,
+    title: 'Norwegian Wood',
+    description: 'This stunning and elegiac novel propelled Haruki Murakami into the forefront of the literary scene.'
+  },
+  {
+    author: Xiaolu Guo,
+    title: 'I Am China',
+    description: 'Rock \'n roll, revolution, and romance are seductively woven together in this intense and moving novel from the author of Twenty Fragments of a Ravenous Youth.'
+  },
+  {
+    author: Lee Chang-Rae,
+    title: 'On Such A Full Sea',
+    description: 'A visionary work from one of the most important writers of our time, On Such a Full Sea takes Chang-rae Lee\'s elegance of prose mastery of storytelling, and insight into themes of identity, culture, work, and love, and lifts them to an exhilarating new plane.'
+  }
+];
+
+function MarketplaceList(){
+  return (
+    <div>
+      <hr/>
+      {masterMarketplaceList.map((book, index) =>
+        <Ticket names={book.author}
+          location={ticket.title}
+          issue={ticket.description}
+          key={index}/>
+      )}
+    </div>
+  );
+}
+
+export default MarketplaceList;
