930a0c4f76ac4eac70fad43fe1005ea2162e5e97
David Mortkowitz <dmortkowitz@gmail.com>
2018-11-30 15:20:58 -0800

    Continuing troublshooting.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,70 +1,19 @@
 import React from 'react';
-import MainNavBar from './MainNavBar';
 import Mainpage from './Mainpage';
-import NewBookList from './NewBookList';
-import NewBookControl from './NewBookControl';
+import Marketplace from './Marketplace';
 import Error404 from './Error404';
 import { Switch, Route } from 'react-router-dom';
-import Moment from 'moment';
-import ListingManager from './ListingManager';
-import { v4 } from 'uuid';
 
-class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      masterBookList: {},
-      selectedBook: null
-    };
-    this.handleAddingNewBookToList = this.handleAddingNewBookToList.bind(this);
-    this.handleChangingSelectedBook = this.handleChangingSelectedBook.bind(this);
-  }
-
-  componentDidMount() {
-    this.waitTimeUpdateTimer = setInterval(() =>
-      this.updateBookElapsedWaitTime(),
-    5000
-    );
-  }
-
-  componentWillUnmount(){
-    clearInterval(this.waitTimeUpdateTimer);
-  }
-
-  updateBookElapsedWaitTime() {
-    var newMasterBookList = Object.assign({}, this.state.masterBookList); Object.keys(newMasterBookList).forEach(bookId => { newMasterBookList[bookId].formattedWaitTime = (newMasterBookList[bookId].timeOpen).fromNow(true);
-    });
-    this.setState({masterBookList: newMasterBookList});
-  }
-
-  handleAddingNewBookToList(newBook){
-    var newBookId = v4();
-    var newMasterBookList = Object.assign({}, this.state.masterBookList, {
-      [newBookId]: newBook
-    });
-    newMasterBookList[newBookId].formattedWaitTime = newMasterBookList[newBookId].timeOpen.fromNow(true);
-    this.setState({masterBookList: newMasterBookList});
-  }
-  
-  handleChangingSelectedBook(bookId){
-    this.setState({selectedBook: bookId});
-  }
-
-  render(){
-    console.log(this.state.masterBookList);
-    return (
-      <div>
-        <Header/>
-        <Switch>
-          <Route exact path='/' render={()=><BookList bookList={this.state.masterBookList} />} />
-          <Route path='/marketplace' render={()=><NewBookControl onNewBookCreation={this.handleAddingNewBookToList} />} />
-          <Route path='/listingmanager' render={(props)=><ListingManager bookList={this.state.masterBookList} currentRouterPath={props.location.pathname} onBookSelection={this.handleChangingSelectedBook}selectedBook={this.state.selectedBook}/>} />        
-          <Route component={Error404} />
-        </Switch>
-      </div>
-    );
-  }
+function App(){
+  return (
+    <div>
+      <Switch>
+      <Route exact path='/' component={Mainpage} />
+      <Route exact path='/marketplace' component={marketplace} />
+      <Route component={Error404} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Marketplace.jsx
+++ src/components/Marketplace.jsx
@@ -1,30 +1,61 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import NewBookList from './NewBookList';
+import NewBookControl from './NewBookControl';
+import Error404 from './Error404';
+import Moment from 'moment';
+import ListingManager from './ListingManager';
+import { v4 } from 'uuid';
 
-function Marketplace(props){
-  if (props.currentRouterPath === '/marketplace'){
-  return (
-    <div onClick={() => {props.onBookSelection(props.BookId);}}>
-    </div>
-  );
-  return (
-    const bookInformation =
+class Marketplace extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterBookList: {},
+      selectedBook: null
+    };
+    this.handleAddingNewBookToList = this.handleAddingNewBookToList.bind(this);
+    this.handleChangingSelectedBook = this.handleChangingSelectedBook.bind(this);
+  }
+
+  componentDidMount() {
+    this.waitTimeUpdateTimer = setInterval(() =>
+      this.updateBookElapsedWaitTime(),
+    5000
+    );
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.waitTimeUpdateTimer);
+  }
+
+  updateBookElapsedWaitTime() {
+    var newMasterBookList = Object.assign({}, this.state.masterBookList); Object.keys(newMasterBookList).forEach(ticketId => { newMasterBookList[ticketId].formattedWaitTime = (newMasterBookList[ticketId].timeOpen).fromNow(true);
+    });
+    this.setState({masterBookList: newMasterBookList});
+  }
+
+  handleAddingNewBookToList(newBook){
+    var newBookId = v4();
+    var newMasterBookList = Object.assign({}, this.state.masterBookList, {
+      [newBookId]: newBook
+    });
+    newMasterBookList[newBookId].formattedWaitTime = newMasterBookList[newBookId].timeOpen.fromNow(true);
+    this.setState({masterBookList: newMasterBookList});
+  }
+
+  handleChangingSelectedBook(ticketId){
+    this.setState({selectedBook: ticketId});
+  }
+
+  render(){
+    console.log(this.state.masterBookList);
+    return (
       <div>
-        <h3>{props.title} - {props.author}</h3>
-        <h4>{props.formattedWaitTime}</h4>
-        <hr/>
-      </div>;
-  );
+        <NewBookList />
+      </div>
+    );
+  }
 }
-
-Marketplace.propTypes = {
-  author: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  description: PropTypes.string,
-  formattedWaitTime: PropTypes.string.isRequired,
-  currentRouterPath: PropTypes.string,
-  onBookSelection: PropTypes.func,
-  bookId: PropTypes.string.isRequired
-};
 
 export default Marketplace;
