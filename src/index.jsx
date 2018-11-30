71b0a29c38da02e5900ffba61d7b0f29dbcb8150
David Mortkowitz <dmortkowitz@gmail.com>
2018-11-30 09:00:25 -0800

    Add all assumed components for Marketplace Statefulness.

--- src/index.jsx
+++ src/index.jsx
@@ -1,32 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './components/App';
-// import { AppContainer } from 'react-hot-loader';
-// import { HashRouter } from 'react-router-dom';
-// // import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-// 
-// 
-// const render = (Component) => {
-//   ReactDOM.render(
-//     <AppContainer>
-//       <HashRouter>
-//         <Component/>
-//       </HashRouter>
-//     </AppContainer>,
-//     document.getElementById('react-app-root')
-//   );
-// };
-// 
-// render(App);
-// 
-// /*eslint-disable */
-// if (module.hot) {
-//   module.hot.accept('./components/App', () => {
-//     render(App);
-//   });
-// }
-// /*eslint-enable */
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
