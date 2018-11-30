d88db7199aa0db231b9f50bc476935f5ddea6f73
David Mortkowitz <dmortkowitz@gmail.com>
2018-11-30 09:26:34 -0800

    Add NewBookForm estimated structure.

--- src/components/NewBookForm.jsx
+++ src/components/NewBookForm.jsx
@@ -1,25 +1,44 @@
-
 import React from 'react';
+import PropTypes from 'prop-types';
+import Moment from 'moment';
 
-function NewBookForm(){
+function NewBookForm(props){  let _author = null;
+  let _title = null;
+  let _description = null;
+
+  function handleNewBookFormSubmission(event) {
+    event.preventDefault();
+    props.onNewBookCreation({author: _author.value, title: _title.value, description: _description.value, timeOpen: new Moment()});
+    _author.value = '';
+    _title.value = '';
+    _description.value = '';
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleNewBookFormSubmission}>
         <input
           type='text'
-          id='author'
-          placeholder='Author Name'/>
+          id='authors'
+          placeholder='Author(s)'
+          ref={(input) => {_author = input;}}/>
         <input
           type='text'
           id='title'
-          placeholder='Title' />
+          placeholder='Title'
+          ref={(input) => {_title = input;}}/>
         <textarea
           id='description'
-          placeholder='Describe your book to sell.' />
+          placeholder='Please give a description of your listing.'
+          ref={(textarea) => {_description = textarea;}}/>
         <button type='submit'>List</button>
       </form>
     </div>
   );
 }
+
+NewBookForm.propTypes = {
+  onNewBookCreation: PropTypes.func
+};
 
 export default NewBookForm;
