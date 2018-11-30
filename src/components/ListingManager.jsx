c55bba0db0962c58489da3360126f15b9b2ec67d
David Mortkowitz <dmortkowitz@gmail.com>
2018-11-30 13:42:17 -0800

    Added ListingManager component content.

---
+++ src/components/ListingManager.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import NewBookList from './NewBookList';
+import NewBookDetail from './NewBookDetail';
+
+function ListingManager(props){
+  let optionalSelectedBookContent = null;
+  if (props.selectedBook != null){
+    optionalSelectedBookContent =  <NewBookDetail selectedBook={props.newBookList[props.selectedBook]}/>;
+  }
+  return (
+    <div>
+      <h2>Listing Manager</h2>
+      {optionalSelectedBookContent}
+      <NewBookList
+        newBookList={props.newBookList}
+        currentRouterPath={props.currentRouterPath}
+        onBookSelection={props.onBookSelection}/>
+    </div>
+  );
+}
+
+ListingManager.propTypes = {
+  newBookList: PropTypes.array,
+  currentRouterPath: PropTypes.string.isRequired,
+  onTicketSelection: PropTypes.func.isRequired,
+  selectedBook: PropTypes.object
+};
+
+export default Admin;
